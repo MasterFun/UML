be248feaafefe2199a18474dc43c0a30b16f624b
MasterFun <christian.kiss12@gmail.com>
2014-09-15 12:57:13 +0200

    add Icons TreeView

--- UML/src/application/Attribut.java
+++ UML/src/application/Attribut.java
@@ -3,10 +3,9 @@ package application;
 import java.io.Serializable;
 
 public class Attribut implements Serializable{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 5546661732545627789L;
+
 	
 	public String getName() {
 		return name;
@@ -14,24 +13,32 @@ public class Attribut implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	
 	public Sichtbarkeit getSichtbar() {
 		return sichtbar;
 	}
 	public void setSichtbar(Sichtbarkeit sichtbar) {
 		this.sichtbar = sichtbar;
 	}
+
+	
 	public String getTyp() {
 		return typ;
 	}
 	public void setTyp(String typ) {
 		this.typ = typ;
 	}
+
+	
 	public String getInitialwert() {
 		return initialwert;
 	}
 	public void setInitialwert(String initialwert) {
 		this.initialwert = initialwert;
 	}
+
+
 	public String getStereotyp() {
 		return stereotyp;
 	}
